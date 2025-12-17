cc8e0e947eeb827652ade831bfcc9c34b79395ac
CodexHubX <111242276+CodexHubX@users.noreply.github.com>
2025-12-17 11:42:00 +0700

    Create Drop.lua

---
+++ Drop.lua
@@ -0,0 +1,15 @@
+task.spawn(function()
+    local ViewReward = require(game:GetService("ReplicatedFirst").Client.UiComponents.ViewReward)
+    local TowerController = require(game:GetService('ReplicatedFirst').Client.Controllers.TowerController)
+    hookfunction(ViewReward, function(v1)
+        table.insert(_G.DropItems, v1)
+    end)
+    local oldP;
+    oldP = hookfunction(TowerController.CheckPlacement, function(...)
+        if _G.PlaceAnywhere then 
+            return true;
+        end;
+        return oldP(...)
+    end)
+    warn("Pass")
+end)
